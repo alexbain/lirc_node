23607098c97403ff5121a4d13c279ff525080de7
Alex Bain <alex@alexba.in>
2013-01-13 16:14:43 -0800

    Cleaning up IRSend tests.

--- test/irsend.js
+++ test/irsend.js
@@ -1,26 +1,29 @@
 var lirc_node = require('../'),
     assert = require('assert'),
-    sinon = require('sinon'),
-    ChildProcess = require('child_process');
+    sinon = require('sinon');
 
 describe('IRSend', function() {
   var irsend = new lirc_node.IRSend();
 
   describe('#_list', function() {
-    it('should have a list function', function() {
-      irsend._list.should.be.an.instanceOf(Function);
+    it('should, by default, return: irsend "" ""', function() {
+      assert.equal(irsend._list(), 'irsend "" ""');
     });
 
-    it("should, by default, return: irsend \"\" \"\"", function() {
-      irsend._list().should.equal("irsend \"\" \"\"")
+    it('should use the remote argument', function() {
+      assert.equal(irsend._list('yamaha'), "irsend yamaha \"\"");
     });
 
-    it('should take a remote argument', function() {
-      irsend._list('yamaha').should.equal("irsend yamaha \"\"");
+    it('should use the remote and code arguments', function() {
+      assert.equal(irsend._list('yamaha', 'KEY_POWER'), 'irsend yamaha KEY_POWER');
     });
+  });
 
-    it('should take a remote and a code argument', function() {
-      irsend._list('yamaha', 'KEY_POWER').should.equal('irsend yamaha KEY_POWER');
+  describe('#list', function() {
+    it('should call the callback', function(done) {
+      irsend.list('yamaha', 'KEY_POWER', function() {
+        done();
+      });
     });
   });
 
