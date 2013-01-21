bd1a718b26acc329f559abf5e247576155a1dcd9
Alex Bain <alex@alexba.in>
2013-01-20 17:15:31 -0800

    Writing a _populateRemoteCommands method.

--- test/lirc_node.js
+++ test/lirc_node.js
@@ -21,5 +21,33 @@ describe('lirc_node', function() {
     it('should exist', function() {
       assert(lirc_node._populateRemotes instanceof Function);
     });
+
+    it('should set exports.remotes as an object', function() {
+        lirc_node._populateRemotes('', '', '');
+        assert(lirc_node.remotes instanceof Object);
+    });
+
+    it('should set exports.remotes to have keys for all remote names', function() {
+        var stdout = "irsend: Yamaha\nirsend: Arizer\nirsend: Microsoft_Xbox360";
+        lirc_node._populateRemotes('', stdout, '');
+        assert(Object.keys(lirc_node.remotes).length === 3);
+        assert(lirc_node.remotes["Yamaha"] instanceof Array);
+        assert(lirc_node.remotes["Arizer"] instanceof Array);
+        assert(lirc_node.remotes["Microsoft_Xbox360"] instanceof Array);
+    });
+  });
+
+  describe('#_populateRemoteCommands', function() {
+    it('should exist', function() {
+      assert(lirc_node._populateRemoteCommands instanceof Function);
+    });
+
+    it('should push commands onto the remote\'s array', function() {
+      var stdout = 'irsend: 0000000000000bd7 OpenClose\nirsend: 0000000000000b9b XboxFancyButton';
+      lirc_node.remotes['Microsoft_Xbox360'] = [];
+      lirc_node._populateRemoteCommands('Microsoft_Xbox360', '', stdout, '');
+      assert(lirc_node.remotes['Microsoft_Xbox360'][0] == 'OpenClose');
+      assert(lirc_node.remotes['Microsoft_Xbox360'][1] == 'XboxFancyButton');
+    });
   });
 });
