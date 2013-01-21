e5c03ef56cd990e620216ad72516f78228bb95e8
Alex Bain <alex@alexba.in>
2013-01-20 18:56:49 -0800

    Cleaning up tests, small callback refactors.

--- test/lirc_node.js
+++ test/lirc_node.js
@@ -1,6 +1,7 @@
 var lirc_node = require('../'),
     assert = require('assert'),
-    sinon = require('sinon');
+    sinon = require('sinon'),
+    q = require('q');
 
 describe('lirc_node', function() {
   describe('reload', function() {
@@ -13,41 +14,64 @@ describe('lirc_node', function() {
       lirc_node.reload(function() {
         assert(spy.called);
         done();
+        spy.restore();
       });
     });
   });
 
-  describe('#_populateRemotes', function() {
-    it('should exist', function() {
-      assert(lirc_node._populateRemotes instanceof Function);
+  describe("Setting an internal list of Remotes", function() {
+    describe('#_populateRemotes', function() {
+      beforeEach(function() {
+        lirc_node.remotes = null;
+      });
+
+      it('should exist', function() {
+        assert(lirc_node._populateRemotes instanceof Function);
+      });
+
+      it('should set exports.remotes as an object', function() {
+          lirc_node._populateRemotes('', '', '');
+          assert(lirc_node.remotes instanceof Object);
+      });
+
+      it('should set exports.remotes to have keys for a single remote name', function() {
+          var stdout = "irsend: SonyTV";
+
+          lirc_node._populateRemotes('', stdout, '');
+
+          assert(Object.keys(lirc_node.remotes).length === 1);
+          assert(lirc_node.remotes["SonyTV"] instanceof Array);
+      });
+
+      it('should set exports.remotes to have keys for all remote names', function() {
+          var stdout = "irsend: Yamaha\nirsend: Arizer\nirsend: Microsoft_Xbox360";
+
+          lirc_node._populateRemotes('', stdout, '');
+
+          assert(Object.keys(lirc_node.remotes).length === 3);
+          assert(lirc_node.remotes["Yamaha"] instanceof Array);
+          assert(lirc_node.remotes["Arizer"] instanceof Array);
+          assert(lirc_node.remotes["Microsoft_Xbox360"] instanceof Array);
+      });
     });
 
-    it('should set exports.remotes as an object', function() {
-        lirc_node._populateRemotes('', '', '');
-        assert(lirc_node.remotes instanceof Object);
-    });
+    describe('#_populateRemoteCommands', function() {
+      beforeEach(function() {
+        lirc_node.remotes = {};
+      });
 
-    it('should set exports.remotes to have keys for all remote names', function() {
-        var stdout = "irsend: Yamaha\nirsend: Arizer\nirsend: Microsoft_Xbox360";
-        lirc_node._populateRemotes('', stdout, '');
-        assert(Object.keys(lirc_node.remotes).length === 3);
-        assert(lirc_node.remotes["Yamaha"] instanceof Array);
-        assert(lirc_node.remotes["Arizer"] instanceof Array);
-        assert(lirc_node.remotes["Microsoft_Xbox360"] instanceof Array);
-    });
-  });
+      it('should exist', function() {
+        assert(lirc_node._populateRemoteCommands instanceof Function);
+      });
 
-  describe('#_populateRemoteCommands', function() {
-    it('should exist', function() {
-      assert(lirc_node._populateRemoteCommands instanceof Function);
-    });
-
-    it('should push commands onto the remote\'s array', function() {
-      var stdout = 'irsend: 0000000000000bd7 OpenClose\nirsend: 0000000000000b9b XboxFancyButton';
-      lirc_node.remotes['Microsoft_Xbox360'] = [];
-      lirc_node._populateRemoteCommands('Microsoft_Xbox360', '', stdout, '');
-      assert(lirc_node.remotes['Microsoft_Xbox360'][0] == 'OpenClose');
-      assert(lirc_node.remotes['Microsoft_Xbox360'][1] == 'XboxFancyButton');
+      it('should push commands onto the remote\'s array', function() {
+        var remote = 'Microsoft_Xbox360';
+        var stdout = 'irsend: 0000000000000bd7 OpenClose\nirsend: 0000000000000b9b XboxFancyButton';
+        lirc_node.remotes[remote] = [];
+        lirc_node._populateRemoteCommands(remote, '', stdout, '');
+        assert(lirc_node.remotes[remote][0] == 'OpenClose');
+        assert(lirc_node.remotes[remote][1] == 'XboxFancyButton');
+      });
     });
   });
 });
