903654063872dfae364737296a71463962789109
Alex Bain <alex@alexba.in>
2016-02-05 19:15:13 -0800

    ESLint + linting everything

--- test/irsend.js
+++ test/irsend.js
@@ -1,168 +1,167 @@
 var IRSend = require('../lib/irsend');
-    assert = require('assert'),
-    sinon = require('sinon'),
-    child_process = require('child_process');
+var assert = require('assert');
+var sinon = require('sinon');
+var childProcess = require('child_process');
 
 require('sinon-as-promised');
 
-describe('IRSend', function() {
-  var execStub = sinon.stub(child_process, 'exec').resolves();
+describe('IRSend', function () {
+  var execStub = sinon.stub(childProcess, 'exec').resolves();
   var irsend = new IRSend(execStub);
 
-  beforeEach(function() {
+  beforeEach(function () {
   });
 
-  afterEach(function() {
+  afterEach(function () {
     execStub.reset();
   });
 
-  describe('#list', function() {
-    it('should call child_process.exec', function(done) {
-      irsend.list('yamaha', 'KEY_POWER').then(function() {
+  describe('#list', function () {
+    it('should call childProcess.exec', function (done) {
+      irsend.list('yamaha', 'KEY_POWER').then(function () {
         assert(execStub.calledOnce);
         done();
       });
     });
 
-    it('should return a promise', function(done) {
-      irsend.list('yamaha', 'KEY_POWER').then(function() { done(); });
+    it('should return a promise', function (done) {
+      irsend.list('yamaha', 'KEY_POWER').then(function () { done(); });
     });
   });
 
-  describe('#send_once', function() {
-    it('should call child_process.exec', function(done) {
-      irsend.send_once('yamaha', 'KEY_POWER').then(function() {
+  describe('#send_once', function () {
+    it('should call childProcess.exec', function (done) {
+      irsend.send_once('yamaha', 'KEY_POWER').then(function () {
         assert(execStub.calledOnce);
         done();
       });
     });
 
-    it('should return a promise', function(done) {
-      irsend.send_once('yamaha', 'KEY_POWER').then(function() { done(); });
+    it('should return a promise', function (done) {
+      irsend.send_once('yamaha', 'KEY_POWER').then(function () { done(); });
     });
   });
 
-  describe('#send_start', function() {
-    it('should call child_process.exec', function(done) {
-      irsend.send_start('yamaha', 'KEY_POWER').then(function() {
+  describe('#send_start', function () {
+    it('should call childProcess.exec', function (done) {
+      irsend.send_start('yamaha', 'KEY_POWER').then(function () {
         assert(execStub.calledOnce);
         done();
       });
     });
 
-    it('should return a promise', function(done) {
-      irsend.send_start('yamaha', 'KEY_POWER').then(function() { done(); });
+    it('should return a promise', function (done) {
+      irsend.send_start('yamaha', 'KEY_POWER').then(function () { done(); });
     });
   });
 
-  describe('#send_stop', function() {
-    it('should call child_process.exec', function(done) {
-      irsend.send_stop('yamaha', 'KEY_POWER').then(function() {
+  describe('#send_stop', function () {
+    it('should call childProcess.exec', function (done) {
+      irsend.send_stop('yamaha', 'KEY_POWER').then(function () {
         assert(execStub.calledOnce);
         done();
       });
     });
 
-    it('should return a promise', function(done) {
-      irsend.send_stop('yamaha', 'KEY_POWER').then(function() { done(); });
+    it('should return a promise', function (done) {
+      irsend.send_stop('yamaha', 'KEY_POWER').then(function () { done(); });
     });
   });
 
-  describe('#simulate', function() {
-    it('should call child_process.exec', function(done) {
-      irsend.simulate('00110101001 BEEP BEEP').then(function() {
+  describe('#simulate', function () {
+    it('should call childProcess.exec', function (done) {
+      irsend.simulate('00110101001 BEEP BEEP').then(function () {
         assert(execStub.calledOnce);
         done();
       });
     });
 
-    it('should return a promise', function(done) {
-      irsend.simulate('00110101001 BEEP BEEP').then(function() { done(); });
+    it('should return a promise', function (done) {
+      irsend.simulate('00110101001 BEEP BEEP').then(function () { done(); });
     });
   });
 
-  describe('#set_transmitters', function() {
-    it('should call child_process.exec', function(done) {
-      irsend.set_transmitters(1).then(function() {
+  describe('#set_transmitters', function () {
+    it('should call childProcess.exec', function (done) {
+      irsend.set_transmitters(1).then(function () {
         assert(execStub.calledOnce);
         done();
       });
     });
 
-    it('should return a promise', function(done) {
-      irsend.set_transmitters(1).then(function() { done(); });
+    it('should return a promise', function (done) {
+      irsend.set_transmitters(1).then(function () { done(); });
     });
   });
 
   // Internal methods
-  describe('#_list', function() {
-    it('should, by default, return: irsend LIST "" ""', function() {
+  describe('#_list', function () {
+    it('should, by default, return: irsend LIST "" ""', function () {
       assert.equal(irsend._list(), 'irsend LIST "" ""');
     });
 
-    it('should use the remote argument', function() {
+    it('should use the remote argument', function () {
       assert.equal(irsend._list('yamaha'), 'irsend LIST "yamaha" ""');
     });
 
-    it('should use the remote and code arguments', function() {
+    it('should use the remote and code arguments', function () {
       assert.equal(irsend._list('yamaha', 'KEY_POWER'), 'irsend LIST "yamaha" "KEY_POWER"');
     });
   });
 
-  describe('#_send_once', function() {
-    it('should generate empty strings on bad arguments', function() {
+  describe('#_send_once', function () {
+    it('should generate empty strings on bad arguments', function () {
       assert.equal(irsend._send_once(), 'irsend SEND_ONCE "" ""');
     });
 
-    it('should generate valid irsend command with remote and code arguments', function() {
+    it('should generate valid irsend command with remote and code arguments', function () {
       assert.equal(irsend._send_once('yamaha', 'KEY_POWER'), 'irsend SEND_ONCE "yamaha" "KEY_POWER"');
     });
 
-    it('should handle an array of codes', function() {
+    it('should handle an array of codes', function () {
       assert.equal(
-        irsend._send_once('yamaha', ['KEY_POWER', 'KEY_VOLUMEUP', 'KEY_VOLUMEDOWN']), 
-        'irsend SEND_ONCE "yamaha" "KEY_POWER" "KEY_VOLUMEUP" "KEY_VOLUMEDOWN"'
-      );
+          irsend._send_once('yamaha', ['KEY_POWER', 'KEY_VOLUMEUP', 'KEY_VOLUMEDOWN']),
+          'irsend SEND_ONCE "yamaha" "KEY_POWER" "KEY_VOLUMEUP" "KEY_VOLUMEDOWN"'
+          );
     });
   });
 
-  describe('#_send_start', function() {
-    it('should generate empty strings on bad arguments', function() {
+  describe('#_send_start', function () {
+    it('should generate empty strings on bad arguments', function () {
       assert.equal(irsend._send_start(), 'irsend SEND_START "" ""');
     });
 
-    it('should generate valid irsend command with remote and code arguments', function() {
+    it('should generate valid irsend command with remote and code arguments', function () {
       assert.equal(irsend._send_start('yamaha', 'KEY_POWER'), 'irsend SEND_START "yamaha" "KEY_POWER"');
     });
   });
 
-  describe('#_send_stop', function() {
-    it('should generate empty strings on bad arguments', function() {
+  describe('#_send_stop', function () {
+    it('should generate empty strings on bad arguments', function () {
       assert.equal(irsend._send_stop(), 'irsend SEND_STOP "" ""');
     });
 
-    it('should generate valid irsend command with remote and code arguments', function() {
+    it('should generate valid irsend command with remote and code arguments', function () {
       assert.equal(irsend._send_stop('yamaha', 'KEY_POWER'), 'irsend SEND_STOP "yamaha" "KEY_POWER"');
     });
   });
 
-  describe('#_set_transmitters', function() {
-    it('should allow for setting 1 transmitter', function() {
+  describe('#_set_transmitters', function () {
+    it('should allow for setting 1 transmitter', function () {
       assert.equal(irsend._set_transmitters(1), 'irsend SET_TRANSMITTERS 1');
     });
 
-    it('should allow for setting 3 transmitters', function() {
+    it('should allow for setting 3 transmitters', function () {
       assert.equal(irsend._set_transmitters([1, 3, 5]), 'irsend SET_TRANSMITTERS 1 3 5');
     });
   });
 
-  describe('#_simulate', function() {
-    it('should forward along a command', function() {
+  describe('#_simulate', function () {
+    it('should forward along a command', function () {
       assert.equal(
-        irsend._simulate("0000000000000476 00 OK TECHNISAT_ST3004S"), 
-        'irsend SIMULATE "0000000000000476 00 OK TECHNISAT_ST3004S"'
-      );
+          irsend._simulate('0000000000000476 00 OK TECHNISAT_ST3004S'),
+          'irsend SIMULATE "0000000000000476 00 OK TECHNISAT_ST3004S"'
+          );
     });
   });
-
 });
--- test/lirc_node.js
+++ test/lirc_node.js
@@ -1,82 +1,81 @@
-var lirc_node = require('../'),
-    assert = require('assert'),
-    sinon = require('sinon'),
-    q = require('q');
+var lircNode = require('../');
+var assert = require('assert');
+var sinon = require('sinon');
 
-describe('lirc_node', function() {
-  describe('init', function() {
-    it('should exist', function() {
-      assert(lirc_node.init instanceof Function);
+describe('lircNode', function () {
+  describe('init', function () {
+    it('should exist', function () {
+      assert(lircNode.init instanceof Function);
     });
   });
 
-  describe("Setting an internal list of Remotes", function() {
-    describe('#_populateRemotes', function() {
-      beforeEach(function() {
-        lirc_node.remotes = {};
+  describe('Setting an internal list of Remotes', function () {
+    describe('#_populateRemotes', function () {
+      beforeEach(function () {
+        lircNode.remotes = {};
       });
 
-      it('should exist', function() {
-        assert(lirc_node._populateRemotes instanceof Function);
+      it('should exist', function () {
+        assert(lircNode._populateRemotes instanceof Function);
       });
 
-      it('should set exports.remotes as an object', function() {
-        lirc_node._populateRemotes(['', '']);
-        assert(lirc_node.remotes instanceof Object);
+      it('should set exports.remotes as an object', function () {
+        lircNode._populateRemotes(['', '']);
+        assert(lircNode.remotes instanceof Object);
       });
 
-      it('should set exports.remotes to have keys for a single remote name', function() {
+      it('should set exports.remotes to have keys for a single remote name', function () {
         var result = ['', 'irsend: SonyTV'];
 
-        lirc_node._populateRemotes(result);
+        lircNode._populateRemotes(result);
 
-        assert(Object.keys(lirc_node.remotes).length === 1);
-        assert(lirc_node.remotes["SonyTV"] instanceof Array);
+        assert(Object.keys(lircNode.remotes).length === 1);
+        assert(lircNode.remotes.SonyTV instanceof Array);
       });
 
-      it('should set exports.remotes to have keys for all remote names', function() {
-          var result = ['', 'irsend: Yamaha\nirsend: Arizer\nirsend: Microsoft_Xbox360'];
+      it('should set exports.remotes to have keys for all remote names', function () {
+        var result = ['', 'irsend: Yamaha\nirsend: Arizer\nirsend: Microsoft_Xbox360'];
 
-          lirc_node._populateRemotes(result);
+        lircNode._populateRemotes(result);
 
-          assert(Object.keys(lirc_node.remotes).length === 3);
-          assert(lirc_node.remotes["Yamaha"] instanceof Array);
-          assert(lirc_node.remotes["Arizer"] instanceof Array);
-          assert(lirc_node.remotes["Microsoft_Xbox360"] instanceof Array);
-      });
-    });
-
-    describe('#_populateCommands', function() {
-      beforeEach(function() {
-        sinon.stub(lirc_node.irsend, 'list', function() {});
-      });
-
-      afterEach(function() {
-        lirc_node.remotes = {};
-        lirc_node.irsend.list.restore();
-      });
-
-      it('should exist', function() {
-        assert(lirc_node._populateCommands instanceof Function);
+        assert(Object.keys(lircNode.remotes).length === 3);
+        assert(lircNode.remotes.Yamaha instanceof Array);
+        assert(lircNode.remotes.Arizer instanceof Array);
+        assert(lircNode.remotes.Microsoft_Xbox360 instanceof Array);
       });
     });
 
-    describe('#_populateRemoteCommands', function() {
-      beforeEach(function() {
-        lirc_node.remotes = {};
+    describe('#_populateCommands', function () {
+      beforeEach(function () {
+        sinon.stub(lircNode.irsend, 'list', function () {});
       });
 
-      it('should exist', function() {
-        assert(lirc_node._populateRemoteCommands instanceof Function);
+      afterEach(function () {
+        lircNode.remotes = {};
+        lircNode.irsend.list.restore();
       });
 
-      it('should push commands onto the remote\'s array', function() {
+      it('should exist', function () {
+        assert(lircNode._populateCommands instanceof Function);
+      });
+    });
+
+    describe('#_populateRemoteCommands', function () {
+      beforeEach(function () {
+        lircNode.remotes = {};
+      });
+
+      it('should exist', function () {
+        assert(lircNode._populateRemoteCommands instanceof Function);
+      });
+
+      it('should push commands onto the remote\'s array', function () {
         var remote = 'Microsoft_Xbox360';
         var result = ['', 'irsend: 0000000000000bd7 OpenClose\nirsend: 0000000000000b9b XboxFancyButton'];
-        lirc_node.remotes[remote] = [];
-        lirc_node._populateRemoteCommands(remote, result);
-        assert(lirc_node.remotes[remote][0] == 'OpenClose');
-        assert(lirc_node.remotes[remote][1] == 'XboxFancyButton');
+        lircNode.remotes[remote] = [];
+        lircNode._populateRemoteCommands(remote, result);
+        assert(lircNode.remotes[remote][0] === 'OpenClose');
+        assert(lircNode.remotes[remote][1] === 'XboxFancyButton');
       });
     });
   });
